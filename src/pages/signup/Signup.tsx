b7e9e3fc1f77f138caa78d4d4f7d1364b55d70ac
Elvis2280 <Elvismiranda924g@gmail.com>
2024-01-15 00:57:42 +0900

    Add Signup page and update dependencies

---
+++ src/pages/signup/Signup.tsx
@@ -0,0 +1,10 @@
+import React, { type ReactElement } from 'react'
+import { Text, View } from 'tamagui'
+
+export default function Signup(): ReactElement {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  )
+}
